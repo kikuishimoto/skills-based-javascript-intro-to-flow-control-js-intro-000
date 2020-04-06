02bd12c403491ae85dbf6598a57f37caa2ff863c
kiku ishimoto <kikuish@gmail.com>
2020-04-06 03:04:34 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -30,11 +30,7 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-  switch(age) {
-    case age>= 13 && <=19:
-      return `You are a teenager`
-    default:
-      return `You have an age`
+
 
   }
   }
